834020cdc66d4a7a408b84dd4cbd5aeeea5ba7c1
ronak-d <ronakdodeja73@gamil.com>
2022-11-15 01:16:27 +0530

    sign in

--- src/Components/Pages/SignIn.jsx
+++ src/Components/Pages/SignIn.jsx
@@ -105,7 +105,7 @@ export default function SignInSide() {
                 variant="contained"
                 sx={{ mt: 3, mb: 2 }}
               >
-                Sign In
+                Login
               </Button>
               {/* <Grid container>
                 <Grid item xs>
